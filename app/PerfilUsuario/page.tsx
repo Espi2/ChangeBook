9ea5fa9f4b7f182278711e89ddd4a2f0d56d65b8
Angel Parada <angelparada@MacBook-Pro-de-Angel.local>
2024-05-18 15:41:53 -0600

    Formulario para editar tu perfil y cambiar el nombre o la foto de perfil

--- app/PerfilUsuario/page.tsx
+++ app/PerfilUsuario/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import BookCard from './cardBook';
+import EditarPerfil from './EditarPerfil'; // Importa el componente EditarPerfil
 import { fetchBooks } from './libro.service';
 import { fetchUser } from './user.service';
 import axios from 'axios';
@@ -17,7 +18,7 @@ interface Book {
   disponible: boolean;
   userNombre: string;
   codigoUsuario: string;
-  imagen: string; // Añadir esta propiedad
+  imagen: string;
 }
 
 interface User {
@@ -32,6 +33,7 @@ interface User {
 const Perfil: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
   const [books, setBooks] = useState<Book[]>([]);
+  const [showEditForm, setShowEditForm] = useState(false); // Nuevo estado para controlar la visibilidad del formulario de edición
 
   useEffect(() => {
     const codigoUsuario = localStorage.getItem("codigoUsuario");
@@ -64,20 +66,30 @@ const Perfil: React.FC = () => {
   const handleDeleteBook = (idLibro: string) => {
     setBooks(books.filter(book => book.idLibro !== idLibro));
   };
-  
- return (
+
+  return (
     <div className="container mx-auto p-4">
       {user && (
         <div className="flex items-center justify-between mb-4">
-          <span className="text-center font-cbookF font-bold text-2xl max-w-44 justify-center text-cbookC-800 opacity-60">
-            {user.codigo}
-            <br />
-            {user.nombre}
-            <br />
-            strikes: {user.strikes}
-          </span>
+          <div className="flex items-center">
+            <img src={user.imagenPerfil} alt="Imagen de perfil" className="w-16 h-16 rounded-full mr-4" />
+            <span className="text-center font-cbookF font-bold text-2xl max-w-44 justify-center text-cbookC-800 opacity-60">
+              {user.codigo}
+              <br />
+              {user.nombre}
+              <br />
+              strikes: {user.strikes}
+            </span>
+          </div>
+          <button
+            onClick={() => setShowEditForm(!showEditForm)}
+            className="bg-blue-500 text-white py-1 px-2 rounded"
+          >
+            Editar Perfil
+          </button>
         </div>
       )}
+      {showEditForm && <EditarPerfil />} {/* Muestra el formulario de edición si showEditForm es true */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {books.map((book) => (
           <BookCard key={book.idLibro} book={book} onDelete={handleDeleteBook} />
